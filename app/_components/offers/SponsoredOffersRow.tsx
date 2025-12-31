88a408774adfce58d8f6e61316ded0750e479221
Plugtour <marcelo.v.ameln@gmail.com>
2025-12-31 16:36:42 -0300

    333333

--- app/_components/offers/SponsoredOffersRow.tsx
+++ app/_components/offers/SponsoredOffersRow.tsx
@@ -164,7 +164,7 @@ export default function SponsoredOffersRow({
 
     const step = () => {
       const rect = el.getBoundingClientRect();
-      const viewportBottom = window.innerHeight + 180;
+      const viewportBottom = window.innerHeight + 150;
       if (rect.bottom > viewportBottom) {
         const delta = rect.bottom - viewportBottom;
         window.scrollBy({ top: delta, behavior: 'smooth' });
